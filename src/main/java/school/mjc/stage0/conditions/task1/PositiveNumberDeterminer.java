c448d06c096362f68fa8599286d60ce5101febb2
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-11 12:40:25 +0400

    git commit -m "Modify ConditionalFalsePrinter.java" -m "Modify ConditionalTruePrinter.java" -m "Modify IntegerComparison.java" -m "Modify NoBracketsPrinter.java" -m "Modify PositiveNumberDeterminer.java"

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+
+        if(numberToBeDetermined > 0) {
+            System.out.println(numberToBeDetermined + "is a possitive number");
+        } else if (numberToBeDetermined < 0 ){
+            System.out.println(numberToBeDetermined + "is a negative number");
+        } else if (numberToBeDetermined == 0) {
+            System.out.println(numberToBeDetermined + "zero");
+
+        }
     }
 }
